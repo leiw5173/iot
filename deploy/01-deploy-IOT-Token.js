b9015d9d408e9f34698455a90a9e28e0d6b5809e
Sam <73590328+leiw5173@users.noreply.github.com>
2024-01-04 11:43:35 +0800

    transfer 1000 IOTC to alice and bob when deploy IOTC smart contract

--- deploy/01-deploy-IOT-Token.js
+++ deploy/01-deploy-IOT-Token.js
@@ -11,18 +11,24 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const NEOX_RPC_URL = process.env.NEOX_RPC_URL;
   const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
   const { deploy } = deployments;
-  let deployer;
+  let deployer, alice, bob, provider;
 
   // Check the network to see if it's a local or a testnet
   if (developmentChains.includes(network.name)) {
     const accounts = await ethers.getSigners();
     deployer = accounts[0];
+    alice = accounts[1];
+    bob = accounts[2];
   } else if (network.name == "neox") {
     const provider = new ethers.JsonRpcProvider(NEOX_RPC_URL);
     deployer = new ethers.Wallet(PRIVATE_KEY, provider);
+    alice = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider);
+    bob = new ethers.Wallet(process.env.PRIVATE_KEY_2, provider);
   } else if (network.name == "goerli") {
     const provider = new ethers.JsonRpcProvider(GOERLI_RPC_URL);
     deployer = new ethers.Wallet(PRIVATE_KEY, provider);
+    alice = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider);
+    bob = new ethers.Wallet(process.env.PRIVATE_KEY_2, provider);
   } else {
     console.log("The network is not supported");
   }
@@ -36,6 +42,20 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     waitConfirmations: 1,
   });
   console.log("IOT Token deployed to:", iotToken.address);
+
+  // send tokens to alice and bob
+  const iotContract = await ethers.getContractAt("Currency", iotToken.address);
+  await iotContract.connect(deployer).transfer(alice.address, 1000);
+  await iotContract.connect(deployer).transfer(bob.address, 1000);
+  console.log("Transfered 1000 IOT Tokens to Alice and Bob");
+  console.log(
+    "Alice's balance: ",
+    (await iotContract.balanceOf(alice.address)).toString()
+  );
+  console.log(
+    "Bob's balance: ",
+    (await iotContract.balanceOf(bob.address)).toString()
+  );
 };
 
 module.exports.tags = ["Currency", "all"];
